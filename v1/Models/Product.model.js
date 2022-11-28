915b31dbdf2084e2d40c1350d4ac6b12d25151a9
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-28 10:59:26 +0600

    post a product

--- v1/Models/Product.model.js
+++ v1/Models/Product.model.js
@@ -13,88 +13,67 @@ const { ObjectId } = mongoose.Schema.Types;
 // Schema Design
 const productSchema = mongoose.Schema({
     name: {
-        type: String,
-        required: [true, "Please Provide a Name for this Product."],
-        trim: true,
-        lowercase: true,
-        unique: [true, "Name must be unique."],
-        minlength: [3, "Name Must be at least 3 Charecters."],
-        maxlength: [100, "Name is too large."],
-
+      type: String,
+      required: [true, "Please provide a name for this product."],
+      trim: true,
+      unique: [true, "Name must be unique"],
+      lowercase: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLenght: [100, "Name is too large"],
     },
     description: {
-        type: String,
-        required: [true, "Please Provide a Description for this Product."],
+      type: String,
+      required: true
     },
+    
     unit: {
-        type: String,
-        required: [true],
-        enum: {
-            values: ["kg", "litre", "pcs", "bag"],
-            message: "Unit value Can't be {VALUE}, must be kg/litre/pcs/bag."
-        },
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs", "bag"],
+        message: "unit value can't be {VALUE}, must be kg/litre/pcs/bag"
+      }
     },
+
     imageURLs: [{
+      type: String,
+      required: true,
+      validate: [validator.isURL, "wrong url"]
+    }],
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    brand: {
+      name: {
         type: String,
         required: true,
-        validate: {
-            validator: (value) => {
-                if (!Array.isArray(value)) {
-                    return false
-                };
-                let isValid = true;
-                value.forEach(url => {
-                    if (!validator.isURL(url)) {
-                        isValid = false
-                    }
-                });
-                return isValid;
-            },
-            message: "Please Provide a Valid Image URL"
-        }
-    }],
-    category: {
-        type: String,
-        required: true
-    },
-    brand: {
-        name: {
-            type: String,
-            required: true
-        },
-        id: {
-            type: ObjectId,
-            ref: 'Brand',
-            required: true
-        }
+      },
+      id: {
+        type: ObjectId,
+        ref: "Brand",
+        required: true,
+      }
     }
+    
+  }, {
+    timestamps: true,
+  })
+  
 
-    // createdAt: {
-    //     type: Date,
-    //     default: Date.now,
-    // },
-    // updatedAt: {
-    //     type: Date,
-    //     default: Date.now,
-    // },
-    // supplier: {
-    //     type: mongoose.Schema.Types.OnjectId,
-    //     ref: "Supplier"
-    // },
-    // categories: [{
-    //     name: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     _id: {
-    //         type: mongoose.Schema.Types.OnjectId,
-    //     }
-    // }],
-
-
-
-
-}, { timestamps: true });
+   productSchema.pre('save',function(next){
+  
+    //this -> 
+     console.log(' Before saving data');
+       if (this.quantity == 0) {
+        this.status = 'out-of-stock'
+      }
+  
+     next()
+   })
+  
 
 // =================================================================
 
